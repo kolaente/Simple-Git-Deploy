551815b05fbbcc33138a0e6e61baa4c673794653
konrad <k@knt.li>
2017-04-28 17:46:10 +0200

    updated script

--- src/Simple-Git-Deploy/deploy.sh
+++ src/Simple-Git-Deploy/deploy.sh
@@ -1,26 +1,45 @@
 #!/bin/bash
 
-while getopts ':k:g:d:' OPTION ; do
+while getopts ':k:g:d:c:' OPTION ; do
   case "$OPTION" in
     k)   KEY=$OPTARG;;
     g)   GIT_URL=$OPTARG;;
     d)   DEPLOY_DIR=$OPTARG;;
+    c)	 DCONTAINER_NAME=$OPTARG;;
     *)   echo "Unknown parameter"
   esac
 done
 
 echo "-----------------------------"
+echo "Started at:"
+date
 echo "Key: ${KEY}"
 echo "Git-Url: ${GIT_URL}"
 echo "Deploy-Dir: ${DEPLOY_DIR}"
+#echo "ssh-agent bash -c 'ssh-add ${KEY}; git clone ${GIT_URL} /tmp/git-deploy'"
 echo "----------------------------"
 
+#mkdir ${DEPLOY_DIR}_dep
 rm -Rf /tmp/git-deploy
 ssh-agent bash -c "ssh-add ${KEY}; git clone ${GIT_URL} /tmp/git-deploy"
-
-rm -Rf /tmp/git-deploy/.git # Remove the Git Folder
-# Empty the deploy-directory without deleting itself
+rm -Rf /tmp/git-deploy/.git
 rm -Rf ${DEPLOY_DIR}/.* 2> /dev/null
 rm -Rf ${DEPLOY_DIR}/*
-# Move the newly downloaded files to the deploy-folder
+#sleep 1
+
 mv -fT /tmp/git-deploy ${DEPLOY_DIR}
+#cp -urfv /tmp/git-deploy/* ${DEPLOY_DIR}/*
+rm -rf /tmp/git-deploy
+
+#echo $DCONTAINER_NAME
+if [ ! -z "$DCONTAINER_NAME" ]
+  then
+    docker restart $DCONTAINER_NAME
+fi
+
+#[! -z "$DCONTAINER_NAME" ] && docker restart "$DCONTAINER_NAME"
+
+
+echo "Finished at:"
+date
+echo "-----------------------------"
